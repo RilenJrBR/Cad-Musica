3dfc73d32fa693e57f27d003ee7b44ffead382eb
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-02-08 17:54:39 -0300

    first commit

---
+++ src/test/java/com/example/cadmusica/CadmusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadmusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
